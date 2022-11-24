5478af5c29b372ea0836c081482bbce56c3892db
AlexFromSiberia <id764g@gmail.com>
2022-11-24 14:27:21 +0700

    Added: - models created - Admin panel configured - superuser

--- stripe_pay/main/admin.py
+++ stripe_pay/main/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import Item, Order, Discount, Tax, Currency
 
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    save_on_top = True
+    list_display = ('id', 'order_price', 'discount', 'tax', )
+    list_display_links = ('id', 'order_price',)
+    # пагинация - по 10 записей на страницу
+    list_per_page = 10
+    # сколько можно максимально показать при нажатии кнопкт show all
+    list_max_show_all = 100
+    # how to show empty values
+    empty_value_display = '-empty-'
+    # То что будет видно при переходе в каждую запись
+    fields = ('id', 'item', 'order_price', 'discount', 'tax', )
+    readonly_fields = ('id',)
+
+
+class TaxAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'tax_size')
+    list_display_links = ('name',)
+    list_per_page = 10
+    empty_value_display = '-empty-'
+    fields = ('id', 'name', 'tax_size')
+    readonly_fields = ('id',)
+
+
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'discount')
+    list_display_links = ('name',)
+    list_per_page = 10
+    empty_value_display = '-empty-'
+    fields = ('id', 'name', 'discount')
+    readonly_fields = ('id',)
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',  'price', 'currency')
+    list_display_links = ('name',)
+    list_per_page = 10
+    empty_value_display = '-empty-'
+    fields = ('id', 'name', 'description', 'price', 'currency')
+    readonly_fields = ('id',)
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Discount, DiscountAdmin)
+admin.site.register(Tax, TaxAdmin)
+admin.site.register(Currency, )
--- stripe_pay/main/models.py
+++ stripe_pay/main/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    currency_type = models.CharField(verbose_name='Payment currency', max_length=5, unique=True, null=False, blank=False)
+
+    def __str__(self):
+        return self.currency_type
+
+
+class Item(models.Model):
+    name = models.CharField(verbose_name='Product name', max_length=100, null=False, blank=False)
+    description = models.CharField(verbose_name='Product description', max_length=500)
+    price = models.FloatField(verbose_name='Price', null=False, blank=False)
+    currency = models.ForeignKey(Currency, on_delete=models.PROTECT,
+                                 help_text="Select currency",
+                                 verbose_name="Pyment currency", null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Discount(models.Model):
+    name = models.CharField(verbose_name='Discount name', max_length=100, null=False, blank=False)
+    discount = models.FloatField(verbose_name='Discount', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tax(models.Model):
+    name = models.CharField(verbose_name='Tax name', max_length=100, blank=False, null=False)
+    tax_size = models.FloatField(verbose_name='Tax size', blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    item = models.ManyToManyField(Item, blank=False, verbose_name='Order')
+    discount = models.ForeignKey(Discount, on_delete=models.PROTECT, verbose_name='Discount', blank=True, null=True)
+    tax = models.ForeignKey(Tax, on_delete=models.PROTECT, verbose_name='Tax size', blank=False, null=False)
+    order_price = models.FloatField(verbose_name='Order price', blank=False, null=False)
+
+    def __str__(self):
+        return str(self.id)
+
+
+# Модель Order, в которой можно объединить несколько Item и
+# сделать платёж в Stripe на содержимое Order c общей стоимостью всех Items
+# Модели Discount, Tax, которые можно прикрепить к модели Order и связать
+# с соответствующими атрибутами при создании платежа в Stripe - в таком случае
+# они корректно отображаются в Stripe Checkout форме.
